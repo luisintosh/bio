e43a21661fe5e74e473455eefcd663216f4ea90c
Luis Mendieta <luis.mendieta@ultra.io>
2023-04-13 11:47:28 -0600

    feat: add yt api

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+// YOUTUBE VIDEO PLAYER
+new YT.Player('backgroundVideo', {
+  videoId: 'DbuebKNKQsQ',
+  playerVars: {
+    start: 9,
+    controls: 0,
+    autoplay: 1,
+    loop: 1,
+    modestbranding: 1,
+    playsinline: 1,
+  },
+  events: {
+    onReady: onPlayerReady,
+  },
+});
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+// YOUTUBE VIDEO PLAYER
