a139cf10d3e2e2b361098287b08b26966902a811
Luis Mendieta <luis.mendieta@ultra.io>
2023-04-13 13:17:09 -0600

    feat: change background

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 import './style.css';
-
-// YOUTUBE VIDEO PLAYER
-new YT.Player('backgroundVideo', {
-  videoId: 'DbuebKNKQsQ',
-  playerVars: {
-    start: 9,
-    controls: 0,
-    autoplay: 1,
-    loop: 1,
-    modestbranding: 1,
-    playsinline: 1,
-  },
-  events: {
-    onReady: onPlayerReady,
-  },
-});
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-// YOUTUBE VIDEO PLAYER
+import './src/particles';
