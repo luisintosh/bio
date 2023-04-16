5abbecc782690d20acea36433850697a8c7ac8f4
Luis Mendieta <luis.mendieta@ultra.io>
2023-04-16 10:20:59 -0600

    feat: add cursor animation

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import './style.css';
 import './src/particles';
+import './src/cursor-trail.min'
+
+// Cursor Trail Effects
+cursorTrail({
+   pattern: 'silverSparkles',
+   animationType: 'rotation90'
+});
